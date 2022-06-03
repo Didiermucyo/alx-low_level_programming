f92e54f60f82c297abda9b765fb83e4c7e971d7a
Didiermucyo <mucyohdidier@gmail.com>
2022-06-03 07:21:50 -0700

    greater than 5

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,18 +15,22 @@ int main(void)
 {
 	int n;
 
+	int last_digit;
+
+	last_digit = n / 10;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* assign the random number */
-	if (n > 5)
+	if (last_digit > 5)
 	{
-		printf("%d and is greater than 5\n", n);
-	} else if (n == 0)
+		printf("%d and is greater than 5\n", last_digit);
+	} else if (last_digit == 0)
 	{
-		printf("%d and is 0\n", n);
-	} else if (n < 6 && n != 0)
+		printf("%d and is 0\n", last_digit);
+
+	} else if (last_digit < 6 && last_digit != 0)
 	{
-		printf("%d and is less than 6 and not 0\n", n);
+		printf("%d and is less than 6 and not 0\n", last_digit);
 	}
 	return (0);
 }
