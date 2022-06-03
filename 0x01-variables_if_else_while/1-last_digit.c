e9915cc352ddfaecb713e12788726f319f1aa381
Didiermucyo <mucyohdidier@gmail.com>
2022-06-03 06:52:32 -0700

    greater than or less than 6 or equal to zero

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,22 +12,22 @@
  *
  */
 int main(void)
-{       
+{
 	int n;
-        
+
 	srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* assign the random number */
-        if (n > 5)
-        {
-                printf("%d and is greater than 5\n", n);
-        } else if (n == 0)
-        {
-                printf("%d and is 0\n", n);
-        } else if (n < 6) && (n != 0)
-        {
-                printf("%d is and is less than 6 and not 0\n", n);
-        }
-        return (0);
+	n = rand() - RAND_MAX / 2;
+	/* assign the random number */
+	if (n > 5)
+	{
+		printf("%d and is greater than 5\n", n);
+	} else if (n == 0)
+	{
+		printf("%d and is 0\n", n);
+	} else if (n < 6 != 0)
+	{
+		printf("%d and is less than 6 and not 0\n", n);
+	}
+	return (0);
 }
 
