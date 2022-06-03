450551b57c06cbf72c477f67991eaace0bfb4c7d
Didiermucyo <mucyohdidier@gmail.com>
2022-06-03 11:51:48 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - all codes
+ * Description:A program that prints alphabets in lowercase followed by a line
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
