7980170d848fa73b70af7b7be835c701cd43719c
Didiermucyo <mucyohdidier@gmail.com>
2022-06-16 06:39:10 -0700

    to uppercases

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *string_toupper - change lowercases letters of a string to upper.
+ *
+ * @str: string to return.
+ * Return: always string.
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
